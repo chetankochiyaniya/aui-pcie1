2b047eee2366d9cf60a202783435364e44ff544f
Chetan Kochiyaniya <ckochiyaniya950@rku.ac.in>
2022-02-03 21:24:24 +0530

    post

--- routes.js
+++ routes.js
@@ -3,10 +3,33 @@ var router = express.Router();
 var Menu = require('./Models/Menu')
 
 
-//to fetch movies
+//to fetch data
 router.get('/foodmenu',async(req,res)=>{
     const foodmenu = await Menu.find()
     res.send(foodmenu)
+})
+
+//to add the data
+router.post("/foodmenu",async(req,res)=>{
+    const foodmenu = new Menu({
+        name:req.body.name,
+        category:req.body.category,
+        price:req.body.price
+    })
+
+    await foodmenu.save((err,msg)=>{
+        if(err){
+            res.status(500).json({
+                "error":err
+            })
+        }
+        else{
+            res.status(200).json({
+                "My-message":msg
+            })
+        }
+    })
+
 })
 
 
